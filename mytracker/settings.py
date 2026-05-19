224244b1ebfbb4b6e6b22530fa925a9f707c3dc9
Your Name <you@example.com>
2026-05-19 22:19:08 +0530

    debug false

--- mytracker/settings.py
+++ mytracker/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rm00$y5tae^s6#@09d*+(_(o=_=tk66x)_ji*8)57#bv)qw%y#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True').lower() in ('1', 'true', 'yes')
 
 ALLOWED_HOSTS = ["*"]
 
